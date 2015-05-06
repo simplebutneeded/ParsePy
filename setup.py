fad9917f08b0afd9ad33d0fd169cc8604b549aba
soby <sobyx@hotmail.com>
2015-05-06 09:52:36 -0700

    removed grequests from setup.py dependencies

--- setup.py
+++ setup.py
@@ -21,16 +21,13 @@ class TestCommand(Command):
 
 setup(
     name='parse_rest',
-    version='0.2.20150427',
+    version='0.2.201500507',
     description='A client library for Parse.com\'.s REST API',
     url='https://github.com/dgrtwo/ParsePy',
     packages=['parse_rest'],
     maintainer='David Robinson',
     maintainer_email='dgrtwo@princeton.edu',
     cmdclass={'test': TestCommand},
-    install_requires=[
-            "grequests",
-        ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
