3b7763f7f3e350b25224a63f3bfef17e95b5ce7a
soby <sobyx@hotmail.com>
2015-12-05 20:47:05 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class TestCommand(Command):
 
 setup(
     name='parse_rest',
-    version='0.2.201512041',
+    version='0.2.20151205',
     description='A client library for Parse.com\'.s REST API',
     url='https://github.com/dgrtwo/ParsePy',
     packages=['parse_rest'],
