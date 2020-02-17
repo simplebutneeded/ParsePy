c15f29ff23b4c334ec042c81d6501e919c1555ea
moden <moden.py@gmail.com>
2020-02-17 11:42:21 +0200

    Enable only logging

--- parse_rest/user.py
+++ parse_rest/user.py
@@ -11,6 +11,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import logging
+logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
 
 from core import ResourceRequestLoginRequired
